c51ba9aeb3676ee061e018a714a3c527482dcb39
vFOXv <sly13fox@gmail.com>
2015-12-24 19:38:49 +0200

    ferst

--- ILR4/src/MinMan/Main.java
+++ ILR4/src/MinMan/Main.java
@@ -2,31 +2,33 @@ package MinMan;
 
 public class Main {
 
-    static int min, max, temp;
+    static int min, minIndex, maxIndex, max, temp;
 
     public static void main(String[] args) {
         Scaner scan = new Scaner();
         RandomArray myArray = new RandomArray();
         int matrix[] = myArray.fullArray(scan.line);
-        for (int i = 0; i < matrix.length ; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             System.out.print(matrix[i] + " ");
         }
-
-        for (int i = 0; i < matrix.length - 1; i++) {
-            if (matrix[i] < matrix[i + 1]) {
-                min = i;
-            }else{
-
+        min = matrix[0];
+        max = matrix[0];
+        for (int i = 1; i < matrix.length; i++) {
+            if (min > matrix[i]) {
+                min = matrix[i];
+                minIndex = i;
             }
-            if (matrix[i] > matrix[i + 1]) {
-                max = i;
+            if (max < matrix[i]) {
+                max = matrix[i];
+                maxIndex = i;
             }
         }
-        temp = matrix[min];
-        matrix[min] = matrix[max];
-        matrix[max] = temp;
+        temp = matrix[minIndex];
+
+        matrix[minIndex] = matrix[maxIndex];
+        matrix[maxIndex] = temp;
         System.out.println(" ");
-        for (int i = 0; i < matrix.length ; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             System.out.print(matrix[i] + " ");
         }
     }
